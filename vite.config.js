35dd7f9d956f96b123b0cacb8590e1be960c67b2
madiera36 <gsaha.1803@gmail.com>
2025-08-25 00:15:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: replace REPO_NAME with your repo (e.g., 'linkedin-scraper-react')
 export default defineConfig({
   plugins: [react()],
-  base: '/linkedin-engagement/',
+  base: '/linkedin-engagement/', // must match your repo name exactly
 })
